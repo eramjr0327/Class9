5c15efdfd4a9ee6dca8ca25701047a913f5f6662
eramjr0327 <90418926+eramjr0327@users.noreply.github.com>
2021-10-07 17:48:47 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+//global variable
+var box 
+
+function setup() {
+  createCanvas(400,400);
+
+  box = createSprite(250,200)
+}
+
+function draw() 
+{
+  background(30);
+
+drawSprites();
+}
+
+
+
+
